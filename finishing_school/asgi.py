d7688fa030b341219b99a0bbddd6374b9a72ab29
Carlos-Aucaruri <carlos.aucaruri@gmail.com>
2024-12-12 22:40:04 -0500

    project is now finishing_school

--- finishing_school/asgi.py
+++ finishing_school/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for softskills project.
+ASGI config for finishing_school project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "softskills.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "finishing_school.settings")
 
 application = get_asgi_application()
--- finishing_school/wsgi.py
+++ finishing_school/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for softskills project.
+WSGI config for finishing_school project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "softskills.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "finishing_school.settings")
 
 application = get_wsgi_application()
