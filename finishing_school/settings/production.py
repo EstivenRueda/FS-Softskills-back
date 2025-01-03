3096024f0a2b4bdd4568e8f2bb210ab6285ddb50
Carlos-Aucaruri <carlos.aucaruri@gmail.com>
2025-01-03 18:23:49 -0500

    feat: added static url to prod config

--- finishing_school/settings/production.py
+++ finishing_school/settings/production.py
@@ -15,5 +15,5 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.getenv("STATIC_ROOT")
-
+STATIC_URL = "/static/"
 MEDIA_ROOT = os.getenv("MEDIA_ROOT")
